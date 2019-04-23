7eb429e2e6c563ab13b1aba10b832ba00369edba
Stijn Aa <stijn.aa@gmail.com>
2019-04-23 17:07:54 +0200

    updated

---
+++ index.js
@@ -0,0 +1,19 @@
+function handleUur() {
+    
+
+    document.querySelector(".bericht").textContent = 'Het weer voor het komende uur. Het gaat over 5 minuten Keihard regenen. echt niet grappig. Maar dat duurt niet lang gelukkig. Daarna is het in ieder geval anderhalf uur droog'
+
+}
+
+function handleDagen() {
+    
+
+    document.querySelector(".bericht").textContent = 'Het weer voor vadaag en morgen. Het is vandaag wisselvallig met ene kleine kans op zon. Vooral korte buien zullen de dag vullen. Het word 15 graden. Morgen word het beter, geen regen en een temperatuur van 19 graden.'
+
+}
+
+function handleWeek() {
+    
+
+    document.querySelector(".bericht").textContent = 'De voorspellingen: maandag 23 april. 23 graden celsius. Met 20% kans op regen. 5mm. dinsdag 24 april. 15 graden celsius. Met 50% kans op regen. 6mm. woensdag 25 april. 12 graden celsius. Met 20% kans op regen. 0mm. donderdag 26 april. 16 graden celsius. Met 10% kans op regen. 2mm. vrijdag 27 april. 19 graden celsius. Met 70% kans op regen. 15mm. zaterdag 28 april. 18 graden celsius. Met 30% kans op regen. 5mm. zondag 29 april. 19 graden celsius. Met 10% kans op regen. 5mm.'
+}
